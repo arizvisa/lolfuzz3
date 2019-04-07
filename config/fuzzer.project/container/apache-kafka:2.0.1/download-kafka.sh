ac67a8decaac3c967df8c1cb658ad65ab8aa4762
Ali Rizvi-Santiago <arizvisa@gmail.com>
2019-04-06 20:45:11 -0500

    Modified the download-kafka.sh script to deal with version 2.1.1 not being mirrored, because Apache has such great fucking timing to change their mirrors.

--- config/fuzzer.project/container/apache-kafka:2.0.1/download-kafka.sh
+++ config/fuzzer.project/container/apache-kafka:2.0.1/download-kafka.sh
@@ -10,6 +10,13 @@ echo "Downloading kafka $MAJOR_VERSION.$MINOR_VERSION"
 if [[ "$MAJOR_VERSION" == "0" && "$MINOR_VERSION" -lt "11" ]]; then
 	echo "Version prior to 0.10.2.1 - downloading direct"
 	url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/${FILENAME}"
+
+## Versions prior to 2.1.1 are not actively mirrored
+elif [[ "$MAJOR_VERSION" == "2" && "$MINOR_VERSION" -lt "1" ]]; then
+	echo "Version prior to 2.1.1 - downloading direct"
+	url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/${FILENAME}"
+
+## Otherwise it should be mirrored
 else
 	url=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/kafka/${KAFKA_VERSION}/${FILENAME}&as_json=1" | jq -r '"\(.preferred)\(.path_info)"')
 fi
